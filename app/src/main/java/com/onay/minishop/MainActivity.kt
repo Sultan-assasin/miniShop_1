f50a00ce20e3bf4a54a3995c85bcb3e179c0d894
Sultan-assasin <sulltanadai@gmail.com>
2022-07-27 17:49:14 +0600

    Start today at 27.7.2022

---
+++ app/src/main/java/com/onay/minishop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.onay.minishop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
