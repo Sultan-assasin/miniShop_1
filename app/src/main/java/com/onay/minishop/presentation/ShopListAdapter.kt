ec7413c2b70496b4d6bec9f354a01031e21cd256
Sultan-assasin <sulltanadai@gmail.com>
2022-08-03 13:20:42 +0600

    lesson_11 . Рабочая версия . Работает долгое нажатие и обычное в которым вызываеться информация о элементе

--- app/src/main/java/com/onay/minishop/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/onay/minishop/presentation/ShopListAdapter.kt
@@ -1,6 +1,5 @@
 package com.onay.minishop.presentation
 
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,20 +9,19 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.onay.minishop.R
 import com.onay.minishop.domain.ShopItem
-import java.lang.RuntimeException
+
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
     var count = 0
-    class ShopItemViewHolder(val view : View): RecyclerView.ViewHolder(view){
-        val tvName = view.findViewById<TextView>(R.id.tv_name)
-        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
-    }
-
-    var ShopList = listOf<ShopItem>()
-    set(value){
-        field = value
-    }
+    var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
+    var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
         Log.d("ShopListAdapter", "onCreateViewHolder, count: ${++count}")
@@ -37,61 +35,55 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
-        val shopItem = ShopList[position]
+        val shopItem = shopList[position]
         viewHolder.view.setOnLongClickListener {
+            onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-            viewHolder.tvName.text =shopItem.name
-            viewHolder.tvCount.text = shopItem.count.toString()
-
-        /*
-        1 способ
-        else
-        {
-            viewHolder.tvName.text =""
-            viewHolder.tvCount.text = ""
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context , android.R.color.white))
+        viewHolder.view.setOnClickListener {
+            onShopItemClickListener?.invoke(shopItem)
         }
-        решение бага в котором существующие view была переисползывана но число 2 может в 17 месте
-         */
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+    }
+
+    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
+        super.onViewRecycled(viewHolder)
+        viewHolder.tvName.text = ""
+        viewHolder.tvCount.text = ""
+        viewHolder.tvName.setTextColor(
+            ContextCompat.getColor(
+                viewHolder.view.context,
+                android.R.color.white
+            )
+        )
     }
 
     override fun getItemCount(): Int {
-        return ShopList.size
+        return shopList.size
     }
 
     override fun getItemViewType(position: Int): Int {
-        val item = ShopList[position]
-        return if(item.enabled){
+        val item = shopList[position]
+        return if (item.enabled) {
             VIEW_TYPE_ENABLED
-        }else{
+        } else {
             VIEW_TYPE_DISABLED
         }
     }
 
-    /*
-    не стоит исползывать
-    причина
-    когда мы вызывали onBindviewhohlder он проверяет совпадает ли viewholder если нет то position .
-    но как так мы определяем position каждый раз viewtype будет у всех разных и он заново создаст
-    новый обьект и мы также вернулись первоначалной проблеме . сколько объектов столько view создастся
-    заново . но они будут создаваться при скроле а не сразу . вот и вся разница
-
-     */
-
-    override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
-        super.onViewRecycled(viewHolder)
-        viewHolder.tvName.text =""
-        viewHolder.tvCount.text = ""
-
-        //установлены значения по умолчанию
-
+    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
     }
+
+
+
     companion object {
-        val VIEW_TYPE_ENABLED = 0
-        val VIEW_TYPE_DISABLED = 1
 
-        const val  MAX_POOL_SIZE = 20
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 101
+
+        const val MAX_POOL_SIZE = 30
     }
-
 }
