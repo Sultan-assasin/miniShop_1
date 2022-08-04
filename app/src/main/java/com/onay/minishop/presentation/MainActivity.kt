ab53bc89d5556ab60e3fb09a99dc06589e8e3c51
Sultan-assasin <sulltanadai@gmail.com>
2022-08-04 23:55:25 +0600

    lesson_11 . Рабочая версия . Работает долгое нажатие и обычное в которым вызываеться информация о элементе . добавлено удаление со свайпом . Причина по которой notifydatacedChange() не стоит исползывать в том что когда происходит какое то изменение заново отрисовывается видимые элеенты . В нашем случае onBindViewHolder вызывается еще 11 раз для видимых элементов . решение для решения данной проблемы мы переопределяем методы в реагирование , то есть например на удаление , изменение . что делает метод можно понять сразу прочитав название метода . Дальше если мы вызваем анимацию старые данные не успеют удалится и вызывается новый список. Для этого предусмотрено метод LifeData который мы исползуем . Мы реагирвуем на новые изминения на LiveData . как понять что элемент удалился или изменился  . Было бы неплохо передать данные на adapter . он каким то образом понимает изминения и мы хотим что бы   adapter сам  вызывает соответствующий методы .

--- app/src/main/java/com/onay/minishop/presentation/MainActivity.kt
+++ app/src/main/java/com/onay/minishop/presentation/MainActivity.kt
@@ -3,7 +3,10 @@ package com.onay.minishop.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.onay.minishop.R
 
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var shopListAdapter : ShopListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setupRecyclerView() {
-        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+       val  rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
 
         with(rvShopList) {
             shopListAdapter = ShopListAdapter()
@@ -43,6 +47,7 @@ class MainActivity : AppCompatActivity() {
         }
         setupLongClickListener()
         setupClickListener()
+        enableSwipeToDeleteAndUndo(rvShopList)
 
         }
     private fun setupLongClickListener() {
@@ -56,5 +61,28 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+   private fun enableSwipeToDeleteAndUndo (recyclerView: RecyclerView){
+       val  rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+       val callback = object : ItemTouchHelper.SimpleCallback(
+           0,
+           ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+       ){
+           override fun onMove(
+               recyclerView: RecyclerView,
+               viewHolder: RecyclerView.ViewHolder,
+               target: RecyclerView.ViewHolder
+           ): Boolean {
+               return false
+           }
+
+           override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+               val item = shopListAdapter.shopList[viewHolder.adapterPosition] // получили обьект
+               viewModel.deleteShopItem(item)
+
+           }
+       }
+       val itemTouchHelper = ItemTouchHelper(callback)
+       itemTouchHelper.attachToRecyclerView(rvShopList)
+   }
 }
 
