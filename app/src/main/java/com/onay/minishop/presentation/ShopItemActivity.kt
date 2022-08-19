9cdb1e1c34dc04c9bbb2b91e4cb74742ca11b91d
Sultan-assasin <sulltanadai@gmail.com>
2022-08-19 22:44:21 +0600

    lesson 20 . Здеь мы дороботали экран что бы он работал во фрагменте . Прошлая ошибка была в том что он имел конструктор , а если значение туда не передать  то он упадет если нету пустого конструктора . Тут мы работаем с аргументами . А параметры вынесли от конструтора . При создании констуртора мы сохраним данные в аргументс . Написали код в типе kotlin.

--- app/src/main/java/com/onay/minishop/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/onay/minishop/presentation/ShopItemActivity.kt
@@ -122,7 +122,6 @@ class ShopItemActivity : AppCompatActivity() {
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
             return intent
         }
-
         fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
             val intent = Intent(context, ShopItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
@@ -130,6 +129,4 @@ class ShopItemActivity : AppCompatActivity() {
             return intent
         }
     }
-
-
 }
